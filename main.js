c82a6235801dd3591df6428f0e4c6c448bbe2e04
elliotrodriguez <elliotrodriguez72@gmail.com>
2018-08-28 14:59:52 -0400

    First version.
    First version

---
+++ main.js
@@ -0,0 +1,86 @@
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+
+function issuesList() {
+    return localStorage.getItem('issues') ? 
+        JSON.parse(localStorage.getItem('issues')) :
+        [];
+}
+
+function fetchIssues(){
+    const issues = this.issuesList();
+    const issuesList = document.getElementById('issuesList');
+
+    issuesList.innerHTML = '';
+
+    if (issues) {
+        issues.forEach(element => {
+            issuesList.innerHTML +=   `<div class="well">
+            <h6>Issue ID:  ${element.id} </h6>
+            <p><span class="label label-info">${element.status}</span></p>
+            <h3>${element.description}</h3>
+            <p><span class="glyphicon glyphicon-time"></span>${element.severity}
+            <span class="glyphicon glyphicon-user"></span>${element.assignedTo}</p>
+            <a href="#" class="btn btn-warning" onclick="setStatusClosed('${element.id}')">Close</a>
+            <a href="#" class="btn btn-danger" onclick="deleteIssue('${element.id}')">Delete</a>
+            </div>`;
+        });
+    }
+}
+
+function saveIssue(e) {
+    const id = chance.guid();
+    const description = document.getElementById('issueDescInput').value || 'No Description Provided';
+    const severity = document.getElementById('issueSeverityInput').value;
+    const assignedTo = document.getElementById('issueAssignedToInput').value || 'No User Assigned.';  
+    const issues =  JSON.parse(localStorage.getItem('issues')) || [];
+
+    issues.push({
+        id,
+        description,
+        severity,
+        assignedTo,
+        status: 'Open'
+      });
+
+    localStorage.setItem('issues', JSON.stringify(issues))
+    document.getElementById('issueInputForm').reset();
+   
+    fetchIssues();
+    
+    e.preventDefault(); 
+  }
+
+  function setStatusClosed (id) {
+    const issues = this.issuesList(); 
+    const updateIssue = issues.find(issueToClose => {
+        return issueToClose.id === id
+    })
+
+    const indOf = issues.indexOf(updateIssue)
+    issues.splice(indOf, 1)
+    issues.push({
+        id : updateIssue.id,
+        description : updateIssue.description,
+        severity : updateIssue.severity,
+        assignedTo : updateIssue.assignedTo,
+        status: 'Closed'
+    })
+ 
+    localStorage.setItem('issues', JSON.stringify(issues));
+    
+    fetchIssues();
+  }
+
+  function deleteIssue (id) {
+    const issues = this.issuesList();
+    const issueToDelete = issues.find(issueToFind => {
+        return issueToFind.id === id
+    });
+    
+    const indOf = issues.indexOf(issueToDelete)
+    issues.splice(indOf, 1)
+    localStorage.setItem('issues', JSON.stringify(issues));
+    
+    fetchIssues();
+  }
+  
